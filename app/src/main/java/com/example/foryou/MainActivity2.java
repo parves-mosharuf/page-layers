e706b339e3e31981ab2ac79a3c3dcb4cfdc7b118
parves-mosharuf <parvesmosharofpappu@gmail.com>
2024-07-01 03:37:32 +0600

    1st commit

--- app/src/main/java/com/example/foryou/MainActivity2.java
+++ app/src/main/java/com/example/foryou/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.foryou;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.second_activity_main);
     }
+    public void onClickMainActivity3(View view) {
+        startActivity(new Intent(this , MainActivity3.class));
+        finish();
+    }
+
+
+
 }
